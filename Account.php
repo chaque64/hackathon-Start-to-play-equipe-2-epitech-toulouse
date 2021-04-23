e9bf22a2560f1a78298d97e2ba90ba6db229ec04
Théoshader <63491603+Snakeshader1@users.noreply.github.com>
2021-04-23 18:40:40 +0200

    1

--- Account.php
+++ Account.php
@@ -1,21 +1,22 @@
+<?php
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=account;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <title>Hyperloop</title>
+    <title>Document</title>
 </head>
 <body>
-    <header>
-        <div id="header">
-            <p id="logo">Hyperloop</p>
-
-        </div>
-    </header>
-
-
 
 </body>
 </html>
